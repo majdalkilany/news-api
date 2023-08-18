5e878074fd6c42ea46147862fc96ea18632528e6
majdalkilany <majdkilany92@gmail.com>
2023-08-19 01:22:24 +0300

    update the api link

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
+
 const app = express();
+
 const PORT = 8080;
 const NEWS_API_URL = 'https://newsapi.org/v2/everything?q=technology&sortBy=popularity&apiKey=3cf6d8ff54464bf3919350313b64b96d';
 
@@ -8,7 +11,6 @@ app.get('/news', async (req, res) => {
     try {
         const response = await axios.get(NEWS_API_URL);
 
-        // Return the data from axios response
         res.send(response.data);
     } catch (error) {
         console.error("Error fetching data:", error);
@@ -16,12 +18,12 @@ app.get('/news', async (req, res) => {
     }
 });
 
-// Add headers to all routes to prevent CORS issues
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
+app.use(cors());
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
