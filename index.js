e7e2c394b563e3519d17174d9a5d1881987beeb3
majdalkilany <majdkilany92@gmail.com>
2023-08-19 01:48:29 +0300

    update the api link

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const app = express();
 
 const PORT = 8080;
 const NEWS_API_URL = 'https://newsapi.org/v2/everything?q=technology&sortBy=popularity&apiKey=3cf6d8ff54464bf3919350313b64b96d';
-
+const corsOptions = {
+    origin:'*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  };
+app.use(cors(corsOptions));
 app.get('/news', async (req, res) => {
     try {
         const response = await axios.get(NEWS_API_URL);
@@ -18,11 +22,7 @@ app.get('/news', async (req, res) => {
     }
 });
 
-const corsOptions = {
-    origin:'*',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  };
-app.use(cors(corsOptions));
+
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
